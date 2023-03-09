9bbe477edc97a86ea798ec5bdbd87f25a5555d3d
Victor <dvicxy@gmail.com>
2023-03-09 14:01:51 +0000

    implement validation of fields

---
+++ lib/components/endpoints.dart
@@ -0,0 +1,12 @@
+class Endpoints {
+  static const String register =
+      "https://choplyfe-api.herokuapp.com/apis/v1/users-service/register"; //register
+  static const String login =
+      "https://choplyfe-api.herokuapp.com/apis/v1/auth-service/login"; //login
+  static const String socialLogin =
+      "https://choplyfe-api.herokuapp.com/apis/v1/auth-service/social-login"; // social login
+  static const String forgotPassword =
+      "https://choplyfe-api.herokuapp.com/apis/v1/auth-service/forgot-password"; //forgot password
+  static const String resetPassword =
+      "https://choplyfe-api.herokuapp.com/apis/v1/auth-service/reset-password"; //reset password
+}
